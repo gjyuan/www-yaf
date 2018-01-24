a20ede64ef53e9a20817a4743989eff41b2c343d
gaojianyuan <gaojianyuan@lianjia.com>
2018-01-24 21:19:39 +0800

    add smarty framework

--- application/app_user/controllers/Index.php
+++ application/app_user/controllers/Index.php
@@ -2,6 +2,7 @@
 class IndexController extends Web_Controller_ApiBase {
 
     public function indexAction() {
+        var_dump(new Vendor_Smarty_Adapter());
         $this->success(Config::get());
     }
 
