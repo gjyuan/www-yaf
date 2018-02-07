8c159997b239fb81db4df66198a32440cd23bcda
gaojianyuan <gaojianyuan@lianjia.com>
2018-02-08 00:05:10 +0800

    add smarty framework to controller base class

--- application/app_user/controllers/Test.php
+++ application/app_user/controllers/Test.php
@@ -2,8 +2,6 @@
 class TestController extends Web_Controller_TplBase {
 
     public function helloAction() {
-        echo "ddd<br>";
-        $this->getView()->assign('name',"gaojiyaun");
         $this->display("hello",['name'=>"gaojiyaun",'list'=>['ddd','sss','qqqq','dfghj']]);
     }
 
